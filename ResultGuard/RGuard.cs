7bf3d738c2d7bad6f98318d873b33de04babda4b
Marvin Drude <marvin@gidd.io>
2024-04-01 11:42:42 +0200

    init package and icon

--- ResultGuard/RGuard.cs
+++ ResultGuard/RGuard.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace ResultGuard;
 
-namespace ResultGuard {
-    internal class RGuard {
+public class RGuard {
+
+    public static RGuard 
+
+    private RGuard() {
+
     }
+
 }
