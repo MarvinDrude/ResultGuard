45aa622d57cd28793dc30c5d4805c6d42406bb43
Marvin Drude <marvin@gidd.io>
2024-04-01 14:20:47 +0200

    Added number extensions

--- ResultGuard/Extensions/RGuardNumberExtensions.cs
+++ ResultGuard/Extensions/RGuardNumberExtensions.cs
@@ -1,8 +1,104 @@
 ﻿
+using System.Numerics;
+
 namespace ResultGuard.Extensions;
 
 public static partial class RGuardExtensions {
 
+    public static bool NegativeOrZero<T, E>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<E> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
 
+        where T : INumberBase<T> {
+
+        if (T.IsZero(argument) || T.IsNegative(argument)) {
+
+            result = Result<E>.FromException(new ArgumentOutOfRangeException(parameterName, message));
+            return true;
+
+        }
+
+        result = null;
+        return false;
+
+    }
+
+    public static bool NegativeOrZero<T>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<T> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
+
+        where T : INumberBase<T> {
+
+        return guard.NegativeOrZero<T, T>(argument, out result, parameterName, message);
+
+    }
+
+    public static bool Zero<T, E>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<E> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
+
+        where T : INumberBase<T> {
+
+        if (T.IsZero(argument)) {
+
+            result = Result<E>.FromException(new ArgumentOutOfRangeException(parameterName, message));
+            return true;
+
+        }
+
+        result = null;
+        return false;
+
+    }
+
+    public static bool Zero<T>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<T> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
+
+        where T : INumberBase<T> {
+
+        return guard.Zero<T, T>(argument, out result, parameterName, message);
+
+    }
+
+    public static bool Negative<T, E>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<E> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
+
+        where T : INumberBase<T> {
+
+        if (T.IsNegative(argument)) {
+
+            result = Result<E>.FromException(new ArgumentOutOfRangeException(parameterName, message));
+            return true;
+
+        }
+
+        result = null;
+        return false;
+
+    }
+
+    public static bool Negative<T>(this RGuard guard,
+        T argument,
+        [MaybeNullWhen(false)] out Result<T> result,
+        [CallerArgumentExpression(nameof(argument))] string? parameterName = null,
+        string? message = null)
+
+        where T : INumberBase<T> {
+
+        return guard.Negative<T, T>(argument, out result, parameterName, message);
+
+    }
 
 }
