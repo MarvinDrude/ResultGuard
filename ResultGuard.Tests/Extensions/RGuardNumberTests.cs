3fe48cbcefa72eade5aee0572060537fca56a94f
Marvin Drude <marvin@gidd.io>
2024-04-06 11:22:21 +0200

    added tests for number extensions

--- ResultGuard.Tests/Extensions/RGuardNumberTests.cs
+++ ResultGuard.Tests/Extensions/RGuardNumberTests.cs
@@ -12,10 +12,10 @@ public sealed class RGuardNumberTests {
     }
 
     [Fact]
-    public void NullIsNullDifferentOutput() {
+    public void NegativeIsNegativeDifferentOutput() {
 
-        object? ob = null;
-        bool ret = RGuard.Is.Null(ob, out Result<bool>? result);
+        int number = -20;
+        bool ret = RGuard.Is.Negative(number, out Result<object>? result);
 
         Assert.True(ret);
         Assert.NotNull(result);
@@ -23,7 +23,229 @@ public sealed class RGuardNumberTests {
         Assert.True(result.IsFailed);
         Assert.NotNull(result.Exception);
 
-        Assert.True(result.Exception is ArgumentNullException);
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeIsZeroDifferentOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.Negative(number, out Result<object>? result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void NegativeIsPositiveDifferentOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.Negative(number, out Result<object>? result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void NegativeIsNegativeSameOutput() {
+
+        int number = -20;
+        bool ret = RGuard.Is.Negative(number, out var result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeIsZeroSameOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.Negative(number, out var result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void NegativeIsPositiveSameOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.Negative(number, out var result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void ZeroIsZeroDifferentOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.Zero(number, out Result<object>? result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void ZeroIsNegativeDifferentOutput() {
+
+        int number = -20;
+        bool ret = RGuard.Is.Zero(number, out Result<object>? result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void ZeroIsPositiveDifferentOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.Zero(number, out Result<object>? result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void ZeroIsZeroSameOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.Zero(number, out var result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void ZeroIsNegativeSameOutput() {
+
+        int number = -20;
+        bool ret = RGuard.Is.Zero(number, out var result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void ZeroIsPositiveSameOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.Zero(number, out var result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsZeroDifferentOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.NegativeOrZero(number, out Result<object>? result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsNegativeDifferentOutput() {
+
+        int number = -20;
+        bool ret = RGuard.Is.NegativeOrZero(number, out Result<object>? result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsPositiveDifferentOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.NegativeOrZero(number, out Result<object>? result);
+
+        Assert.False(ret);
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsZeroSameOutput() {
+
+        int number = 0;
+        bool ret = RGuard.Is.NegativeOrZero(number, out var result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsNegativeSameOutput() {
+
+        int number = -20;
+        bool ret = RGuard.Is.NegativeOrZero(number, out var result);
+
+        Assert.True(ret);
+        Assert.NotNull(result);
+
+        Assert.True(result.IsFailed);
+        Assert.NotNull(result.Exception);
+
+        Assert.True(result.Exception is ArgumentOutOfRangeException);
+
+    }
+
+    [Fact]
+    public void NegativeOrZeroIsPositiveSameOutput() {
+
+        int number = 10;
+        bool ret = RGuard.Is.NegativeOrZero(number, out var result);
+
+        Assert.False(ret);
+        Assert.Null(result);
 
     }
 
