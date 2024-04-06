ec98f8261971e7bbd201782c87ff8af353cc11fb
Marvin Drude <marvin@gidd.io>
2024-04-06 13:21:38 +0200

    Changed example

--- ResultGuard.Testing/Program.cs
+++ ResultGuard.Testing/Program.cs
@@ -3,7 +3,7 @@ using ResultGuard;
 
 Func<string?, Result<string>> simpleUsageToLower = (string? parameter) => {
 
-    if(RGuard.Is.Null(parameter, out var error)) {
+    if (RGuard.Is.Null(parameter, out var error)) {
         return error;
     }
 
@@ -13,7 +13,11 @@ Func<string?, Result<string>> simpleUsageToLower = (string? parameter) => {
 
 Func<string?, Result<MoreUsageToLower>> moreUsageToLower = (string? parameter) => {
 
-    if(RGuard.Is.NullOrEmpty(parameter, out Result<MoreUsageToLower>? error)) {
+    if (RGuard.Is.NullOrEmpty(parameter, out Result<MoreUsageToLower>? error)) {
+        return error;
+    }
+
+    if (RGuard.Is.GreaterThan(parameter.Length, 20, out error, "ParameterName", "Parameter is longer than 20 characters.")) {
         return error;
     }
 
